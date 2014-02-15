06ebb924b87d0ea68ffa2ca4983f608d79c2645e
Brittany Vacchiano <bvacchia@purdue.edu>
2014-02-15 15:07:02 -0500

    Ashley's Wedding Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wedding::Application.config.secret_key_base = '54512d2c150f5a8b00dd22fb5e526640812c52b2dcea070e456b55a9f488c3d212d1bc2d9d5d5dadfcdf99fdf212e4fa84db212d6aeaaaf4546664ac6dfb059f'
